d5954f80f3ab3e3df457819602cb22ec70090e9b
dainiuxt <dainiuxt@gmail.com>
2020-07-15 17:50:00 +0300

    Realized one to many associations between users and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+	belongs_to :user
 	validates :title, presence: true, length: { minimum: 1, maximum: 123 }
 	validates :description, presence: true, length: { minimum: 1, maximum: 500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+	has_many :articles
 	validates :username, presence: true,
-	uniquness: { case_sensitive: false },
+	uniqueness: { case_sensitive: false },
 	length: { minimum: 2, maximum: 51 }
 	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true,
-	uniquness: { case_sensitive: false },
-	length: { maximum: 110 }
+	uniqueness: { case_sensitive: false },
+	length: { maximum: 110 },
 	format: { with: VALID_EMAIL }
 end
