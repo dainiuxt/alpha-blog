b37ad8742d2606f3da32bc04493ec3e3f967ecc2
dainiuxt <dainiuxt@gmail.com>
2020-07-15 16:06:57 +0300

    Added user vlidation functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-
+	validates :username, presence: true,
+	uniquness: { case_sensitive: false },
+	length: { minimum: 2, maximum: 51 }
+	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true,
+	uniquness: { case_sensitive: false },
+	length: { maximum: 110 }
+	format: { with: VALID_EMAIL }
 end
